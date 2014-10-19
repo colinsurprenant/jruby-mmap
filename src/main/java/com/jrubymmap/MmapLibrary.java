ae213417f71b8b1dc06ef4bf01498d5a95bf6890
Colin Surprenant <colin.surprenant@gmail.com>
2014-10-18 22:17:42 -0400

    new Mmap::ByteBuffer namespace and refactor method signatures

--- src/main/java/com/jrubymmap/MmapLibrary.java
+++ src/main/java/com/jrubymmap/MmapLibrary.java
@@ -14,6 +14,7 @@ import org.jruby.RubyObject;
 import org.jruby.RubyString;
 import org.jruby.RubyFixnum;
 import org.jruby.RubyInteger;
+import org.jruby.util.ByteList;
 import org.jruby.anno.JRubyClass;
 import org.jruby.anno.JRubyMethod;
 import org.jruby.runtime.ObjectAllocator;
@@ -23,30 +24,32 @@ import org.jruby.runtime.load.Library;
 
 public class MmapLibrary implements Library {
     public void load(Ruby runtime, boolean wrap) throws IOException {
-        RubyClass mmapClass = runtime.defineClass("Mmap", runtime.getObject(),  new ObjectAllocator() {
+        RubyModule mmapModule = runtime.defineModule("Mmap");
+        RubyClass byteBufferClass = runtime.defineClassUnder("ByteBuffer", runtime.getObject(),  new ObjectAllocator() {
             public IRubyObject allocate(Ruby runtime, RubyClass rubyClass) {
-                return new Mmap(runtime, rubyClass);
+                return new ByteBuffer(runtime, rubyClass);
             }
-        });
-        mmapClass.defineAnnotatedMethods(Mmap.class);
+        }, mmapModule);
+        byteBufferClass.defineAnnotatedMethods(ByteBuffer.class);
     }
 
-    @JRubyClass(name = "Mmap", parent = "Object")
-    public static class Mmap extends RubyObject {
+    @JRubyClass(name = "ByteBuffer", parent = "Object")
+    public static class ByteBuffer extends RubyObject {
 
         private long size;
         private FileChannel channel;
         private MappedByteBuffer buffer;
 
-        public Mmap(Ruby runtime, RubyClass klass) {
+        public ByteBuffer(Ruby runtime, RubyClass klass) {
             super(runtime, klass);
         }
 
+        // def initialize(path, size)
         @JRubyMethod(name = "initialize", required = 2)
         public IRubyObject initialize(ThreadContext context, RubyString path, IRubyObject size)
             throws IOException
         {
-            this.size = ((RubyFixnum)size).getLongValue();
+            this.size = RubyNumeric.num2long(size);
             File file = new File(path.decodeString());
             RandomAccessFile raf = new RandomAccessFile(file, "rw");
             this.channel = raf.getChannel();
@@ -71,69 +74,128 @@ public class MmapLibrary implements Library {
         public void set_position(IRubyObject pos)
             throws IOException
         {
-            this.buffer.position((int)((RubyFixnum)pos).getLongValue());
+            this.buffer.position(RubyNumeric.num2int(pos));
         }
 
-        @JRubyMethod(name = "put_int", required = 1)
-        public void put_int(IRubyObject value) {
-            this.buffer.putInt((int)((RubyFixnum)value).getLongValue());
+        // def put_int(value)
+        // def put_int(value, index)
+        @JRubyMethod(name = "put_int", required = 1, optional = 1)
+        public void put_int(IRubyObject[] args) {
+            int value = RubyNumeric.num2int(args[0]);
+            if (args.length > 1) {
+                int index = RubyNumeric.num2int(args[1]);
+                this.buffer.putInt(index, value);
+            } else {
+                this.buffer.putInt(value);
+            }
         }
 
-        @JRubyMethod(name = "put_int_at", required = 2)
-        public void put_int_at(IRubyObject index, IRubyObject value) {
-            this.buffer.putInt((int)((RubyFixnum)index).getLongValue(), (int)((RubyFixnum)value).getLongValue());
-        }
+        // @JRubyMethod(name = "put_int_at", required = 2)
+        // public void put_int_at(IRubyObject index, IRubyObject value) {
+        //     this.buffer.putInt((int)((RubyFixnum)index).getLongValue(), (int)((RubyFixnum)value).getLongValue());
+        // }
 
-        @JRubyMethod(name = "get_int_at", required = 1)
-        public IRubyObject get_int_at(ThreadContext context, IRubyObject index) {
+        // @JRubyMethod(name = "get_int_at", required = 1)
+        // public IRubyObject get_int_at(ThreadContext context, IRubyObject index) {
+        //     Ruby runtime = context.runtime;
+        //     return runtime.newFixnum(this.buffer.getInt((int)((RubyFixnum)index).getLongValue()));
+        // }
+
+        // def get_int
+        // def get_int(index)
+        @JRubyMethod(name = "get_int", required = 0, optional = 1)
+        public IRubyObject get_int(ThreadContext context, IRubyObject[] args) {
             Ruby runtime = context.runtime;
-            return runtime.newFixnum(this.buffer.getInt((int)((RubyFixnum)index).getLongValue()));
+            if (args.length > 0) {
+                int index = RubyNumeric.num2int(args[0]);
+                return runtime.newFixnum(this.buffer.getInt(index));
+            } else {
+                return runtime.newFixnum(this.buffer.getInt());
+            }
         }
 
-        @JRubyMethod(name = "get_int")
-        public IRubyObject get_int(ThreadContext context) {
+        // def put_long(value)
+        // def put_long(value, index)
+        @JRubyMethod(name = "put_long", required = 1, optional = 1)
+        public void put_long(IRubyObject[] args) {
+            // long value = ((RubyFixnum)args[0]).getLongValue();
+            long value = RubyNumeric.num2long(args[0]);
+            if (args.length > 1) {
+                int index = RubyNumeric.num2int(args[1]);
+                this.buffer.putLong(index, value);
+            } else {
+                this.buffer.putLong(value);
+            }
+        }
+
+        // def get_long
+        // def get_long(index)
+        @JRubyMethod(name = "get_long", required = 0, optional = 1)
+        public IRubyObject get_long(ThreadContext context, IRubyObject[] args) {
             Ruby runtime = context.runtime;
-            return runtime.newFixnum(this.buffer.getInt());
+            if (args.length > 0) {
+                int index = RubyNumeric.num2int(args[0]);
+                return runtime.newFixnum(this.buffer.getLong(index));
+            } else {
+                return runtime.newFixnum(this.buffer.getLong());
+            }
         }
 
-        @JRubyMethod(name = "put_long", required = 1)
-        public void put_long(IRubyObject value) {
-            this.buffer.putLong(((RubyFixnum)value).getLongValue());
+        // relative put the direct underlying bytes without copy
+        //
+        // def put_bytes(string)
+        // def put_bytes(string, offset, length)
+        @JRubyMethod(name = "put_bytes", required = 1, optional = 2)
+        public void put_bytes(ThreadContext context, IRubyObject[] args) {
+            if (args.length == 3) {
+                int offset = RubyNumeric.num2int(args[1]);
+                int length = RubyNumeric.num2int(args[2]);
+                this.buffer.put(((RubyString)args[0]).getByteList().unsafeBytes(), offset, length);
+            } else if (args.length == 1) {
+                ByteList byteList = ((RubyString)args[0]).getByteList();
+                // byteList may have a backing byte[] longer than the actual string bytes
+                // make sure to use only 0..byteList.length()
+                this.buffer.put(byteList.unsafeBytes(), 0, byteList.length());
+            } else {
+                throw context.runtime.newArgumentError("Invalid number of parameters");
+            }
         }
 
-        @JRubyMethod(name = "put_long_at", required = 2)
-        public void put_long_at(IRubyObject index, IRubyObject value) {
-            this.buffer.putLong((int)((RubyFixnum)index).getLongValue(), ((RubyFixnum)value).getLongValue());
+        // relative put a copy of the underlying bytes
+        //
+        // def put_bytes_copy(string)
+        // def put_bytes_copy(string, offset, length)
+        @JRubyMethod(name = "put_bytes_copy", required = 1, optional = 2)
+        public void put_bytes_copy(ThreadContext context, IRubyObject[] args) {
+            if (args.length == 3) {
+                int offset = RubyNumeric.num2int(args[1]);
+                int length = RubyNumeric.num2int(args[2]);
+                this.buffer.put(((RubyString)args[0]).getBytes(), offset, length);
+            } else if (args.length == 1) {
+                // getBytes() copies the bytes in a new byte[] size of the exact string bytesize
+                this.buffer.put(((RubyString)args[0]).getBytes());
+            } else {
+                throw context.runtime.newArgumentError("Invalid number of parameters");
+            }
         }
 
-        @JRubyMethod(name = "get_long_at", required = 1)
-        public IRubyObject get_long_at(ThreadContext context, IRubyObject index) {
-            Ruby runtime = context.runtime;
-            return runtime.newFixnum(this.buffer.getLong((int)((RubyFixnum)index).getLongValue()));
-        }
+        // def get_bytes(size)
+        // def get_bytes(size, offset, length)
+        @JRubyMethod(name = "get_bytes", required = 1, optional = 2)
+        public IRubyObject get_bytes(ThreadContext context, IRubyObject[] args) {
+            // byte[] bytes = new byte[(int)((RubyFixnum)args[0]).getLongValue()];
+            byte[] bytes = new byte[RubyNumeric.num2int(args[0])];
 
-        @JRubyMethod(name = "get_long")
-        public IRubyObject get_long(ThreadContext context) {
-            Ruby runtime = context.runtime;
-            return runtime.newFixnum(this.buffer.getLong());
-        }
+            if (args.length == 3) {
+                int offset = RubyNumeric.num2int(args[1]);
+                int length = RubyNumeric.num2int(args[2]);
+                this.buffer.get(bytes, offset, length);
+            } else if (args.length == 1) {
+                this.buffer.get(bytes);
+            } else {
+                throw context.runtime.newArgumentError("Invalid number of parameters");
+            }
 
-        // put the direct underlying bytes without copy
-        @JRubyMethod(name = "put_bytes", required = 1)
-        public void put_bytes(RubyString data) {
-            this.buffer.put(data.getByteList().unsafeBytes());
-        }
-
-        // put a copy of the underlying bytes
-        @JRubyMethod(name = "put_bytes_copy", required = 1)
-        public void put_bytes_copy(RubyString data) {
-            this.buffer.put(data.getByteList().bytes());
-        }
-
-        @JRubyMethod(name = "get_bytes", required = 1)
-        public IRubyObject get_bytes(ThreadContext context, IRubyObject size) {
-            byte[] bytes = new byte[(int)((RubyFixnum)size).getLongValue()];
-            this.buffer.get(bytes);
             return RubyString.newString(context.runtime, bytes);
         }
 
